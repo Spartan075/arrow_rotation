928e78b5581cb2d7f774375d7062b903f3b9bc82
Spartan075 <andrewcoole1996@gmail.com>
2020-07-05 20:57:47 -0500

    Latest Files

--- arrow_rotation.js
+++ arrow_rotation.js
@@ -1,31 +1,33 @@
-
 const mod = 'arrow_rotation';
 const modDisplayName = "Arrow Rotation"
 
 
-import "foundry.js"
+Hooks.on("ready", function () {
+    KeyboardManager.MOVEMENT_KEYS = {
+        w: ["up"],
+        a: ["left"],
+        s: ["down"],
+        d: ["right"],
+        W: ["up"],
+        A: ["left"],
+        S: ["down"],
+        D: ["right"],
+        ArrowUp: [],
+        ArrowRight: [],
+        ArrowDown: [],
+        ArrowLeft: [],
+        Numpad1: ["down", "left"],
+        Numpad2: ["down"],
+        Numpad3: ["down", "right"],
+        Numpad4: ["left"],
+        Numpad6: ["right"],
+        Numpad7: ["up", "left"],
+        Numpad8: ["up"],
+        Numpad9: ["up", "right"],
+    };
+    
+});
 
-KeyboardManager.MOVEMENT_KEYS = {
-  w: ["up"],
-  a: ["left"],
-  s: ["down"],
-  d: ["right"],
-  W: ["up"],
-  A: ["left"],
-  S: ["down"],
-  D: ["right"],
-  ArrowUp: [],
-  ArrowRight: [],
-  ArrowDown: [],
-  ArrowLeft: [],
-  Numpad1: ["down", "left"],
-  Numpad2: ["down"],
-  Numpad3: ["down", "right"],
-  Numpad4: ["left"],
-  Numpad6: ["right"],
-  Numpad7: ["up", "left"],
-  Numpad8: ["up"],
-  Numpad9: ["up", "right"],
-};
-
-console.log("Hello Foundry!");
+KeyboardManager.getKey(event) {
+    console.log(event.code);
+}

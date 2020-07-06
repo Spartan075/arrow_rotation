4dd2fc6d8916bdef9900d8c36f131fa4dd28142c
MrXenark <mrxenark@gmail.com>
2020-07-06 07:34:41 -0400

    Update arrow_rotation.js

---
+++ arrow_rotation.js
@@ -0,0 +1,35 @@
+const mod = 'arrow_rotation';
+const modDisplayName = "Arrow Rotation"
+
+
+
+    KeyboardManager.MOVEMENT_KEYS = {
+        w: ["up"],
+        a: ["left"],
+        s: ["down"],
+        d: ["right"],
+        W: ["up"],
+        A: ["left"],
+        S: ["down"],
+        D: ["right"],
+        ArrowUp: [],
+        ArrowRight: [],
+        ArrowDown: [],
+        ArrowLeft: [],
+        Numpad1: ["down", "left"],
+        Numpad2: ["down"],
+        Numpad3: ["down", "right"],
+        Numpad4: ["left"],
+        Numpad6: ["right"],
+        Numpad7: ["up", "left"],
+        Numpad8: ["up"],
+        Numpad9: ["up", "right"],
+    };
+    
+
+
+$(document).keydown(function (event) {
+
+    console.log(event.code);
+
+});

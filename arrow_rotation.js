5082ea93ea25d591eb222cd7232dd48c88f9e3ec
MrXenark <mrxenark@gmail.com>
2020-07-06 07:52:38 -0400

    Update arrow_rotation.js

--- arrow_rotation.js
+++ arrow_rotation.js
@@ -12,8 +12,8 @@ const modDisplayName = "Arrow Rotation"
         A: ["left"],
         S: ["down"],
         D: ["right"],
-        ArrowUp: [],
-        ArrowRight: [],
+        ArrowUp: ["Red"],
+        ArrowRight: ["Blue"],
         ArrowDown: [],
         ArrowLeft: [],
         Numpad1: ["down", "left"],
@@ -26,24 +26,6 @@ const modDisplayName = "Arrow Rotation"
         Numpad9: ["up", "right"],
     };
     
-	_handleMovement(event, layer) {
-    if ( !this._moveKeys.size ) return;
-    // Get controlled objects
-    let objects = layer.placeables.filter(o => o._controlled);
-    if ( objects.length === 0 ) return;
-    // Define movement offsets and get moved directions
-    const directions = this._moveKeys;
-    let dx = 0;
-    let dy = 0;
-    // Assign movement offsets
-    if ( directions.has("left") ) dx -= 1;
-    if ( directions.has("up") ) dy -= 1;
-    if ( directions.has("right") ) dx += 1;
-    if ( directions.has("down") ) dy += 1;
-    this._moveKeys.clear();
-    // Perform the shift or rotation
-    layer.moveMany({dx, dy, rotate: event.shiftKey});
-  }
 
 $(document).keydown(function (event) {
 
